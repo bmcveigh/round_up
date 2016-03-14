570497dea2cd070a2acbbfeb79acbfa86a179721
Brian McVeigh <brian.mcveigh@cgi.com>
2016-03-14 11:38:38 -0400

    Reusing jQuery object.

--- round_up.js
+++ round_up.js
@@ -4,15 +4,16 @@
 (function ($) {
 	$(document).ready(function () {
 		$('.roundup-textfield').keyup(function () {
+			var $self = $(this);
 			// Round each emission value to up to 2 decimal places.
-			var number = $('.roundup-textfield').val();
+			var number = $self.val();
 			var pieces = number.split('.');
 			var value;
 
 			if (pieces.length > 1) {
 			    if (pieces[1].length > 2) {
 			        value = Math.round(parseFloat(this.value) * 100) / 100;
-					$('.roundup-textfield').val(value);        
+					$self.val(value);        
 			    }
 			}
 		});
